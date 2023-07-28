50ba43efdaa130733e2b95c72e42683f7a42941b
Kim Eungtae <dotdash09@gmail.com>
2023-07-28 23:32:30 +0900

    코드 정리

--- Program.cs
+++ Program.cs
@@ -117,8 +117,8 @@ public class Program
                         continue;
                     }
 
-                    string albumTitleFix = string.Join("_", album.Title.Split(GetInvalidFileNameChars()));
-                    string trackTitleFix = string.Join("_", track.Title.Split(GetInvalidFileNameChars()));
+                    string albumTitleFix = string.Join("_", album.Title.Split(PathExtension.GetInvalidFileNameChars()));
+                    string trackTitleFix = string.Join("_", track.Title.Split(PathExtension.GetInvalidFileNameChars()));
 
                     string jobPath = $"{tmpDir}/{albumTitleFix}_{album.BrowseId}/{trackTitleFix}_{track.VideoId}";
 
@@ -193,13 +193,4 @@ public class Program
             }
         }
     }
-
-    public static char[] GetInvalidFileNameChars() => new char[]
-    {
-        '\"', '<', '>', '|', '\0',
-        (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
-        (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
-        (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
-        (char)31, ':', '*', '?', '\\', '/'
-    };
 }
